f6da6bb338ef5c05b3caa56404451b890e7a05d8
Roland Angerer <r.angerer@yelsterdigital.com>
2014-04-25 10:51:10 +0200

    added script to print numbers from 1 to 1000

---
+++ csv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from numbers2words import numbers2words
+
+print "zoi,nom"
+for number in range(1, 1000):
+    try:
+        word = numbers2words(number)
+    except Exception as e:
+        word = "Exception(%s)" % type(e).__name__
+
+    print "%d,%s" % (number, word)
