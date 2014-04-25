ff77f16c6def557ff8a508b1219e1d33d61b17ca
Clemens Kaposi <clemens@kaposi.name>
2014-04-25 07:15:50 +0200

    Add coding dojo outcome

--- numbers2words.py
+++ numbers2words.py
@@ -1,8 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def numbers2words():
-    pass
+specials = {
+    1: "oans", 2: "zwoa", 3: "drei",
+    4: "viere", 5: "fünfe", 6: "sechse",
+    7: "sieme", 8: "achte", 9: "neine",
+    10: "zehne", 11: "eife", 12: "zweife",
+    13: "dreizen"
+}
+
+tens = ['zehne', 'zwoanzge', 'dreißge', 'vierzge', 'fuchzge', 'sechzge',
+        'siebzge', 'achtzge', 'neinzge']
+
+values_ez = ['oana', 'zwoara', 'dreia', 'viera', 'fünfa', 'sechsa', 'simma',
+             'ochta', 'neina']
+
+values_tz = ['zen','zwanzg', 'dreißg', 'vierzg', 'fuchzg', 'sechzg',
+        'siebzg', 'dochzg', 'neinzg']
+
+values_hz = ['hundat','zwoahundat','dreihundat','viahundat','fünfhundat',
+             'sechshundat', 'siebnhundat','ochthundat','neihundat']
+
+def numbers2words(number):
+    output = ''
+
+    if number < 0:
+        raise ValueError('is zkloa')
+
+    if number > 100:
+        hz, number = divmod(number,100)
+        output += values_hz[hz - 1]
+
+    if number > 13:
+        if number % 10 == 0:
+            output += tens[number / 10 - 1]
+        else:
+            tz, ez = divmod(number,10)
+            output += values_ez[ez-1] + values_tz[tz-1]
+    else:
+        output = specials[number]
+    return output
 
 if __name__ == "__main__":
     import nose
--- test_numbers2words.py
+++ test_numbers2words.py
@@ -5,5 +5,54 @@ from nose.tools import *
 
 from numbers2words import numbers2words
 
-def test_numbers2words():
-    pass
+values = { 3: "drei",
+           4: "viere",
+           5: "fünfe",
+           6: "sechse",
+           7: "sieme",
+           8: "achte",
+           9: "neine"  }
+
+tens = ['zehne', 'zwoanzge', 'dreißge', 'vierzge', 'fuchzge', 'sechzge',
+        'siebzge', 'achtzge', 'neinzge']
+
+def test_numbers2words_1():
+    assert_equals(numbers2words(1), "oans")
+
+def test_numbers2words_2():
+    assert_equals(numbers2words(2), "zwoa")
+
+def test_numbers2words_10():
+    assert_equals(numbers2words(10), "zehne")
+
+def test_numbers2words_hash():
+    for key,val in values.items():
+        assert_equals(numbers2words(key), val)
+
+def test_numbers2words_tens():
+    for index, val in enumerate(tens, 1):
+        assert_equals(numbers2words(10 * index), val)
+
+def test_numbers2words_11():
+    assert_equals(numbers2words(11), "eife")
+
+def test_numbers2words_12():
+    assert_equals(numbers2words(12), "zweife")
+
+def test_numbers2words_13():
+    assert_equals(numbers2words(13), "dreizen")
+
+def test_numbers2words_21():
+    assert_equals(numbers2words(21), "oanazwanzg")
+
+def test_numbers2words_33():
+    assert_equals(numbers2words(33), "dreiadreißg")
+
+def test_numbers2words_121():
+    assert_equals(numbers2words(121), "hundatoanazwanzg")
+
+def test_numbers2words_285():
+    assert_equals(numbers2words(285), "zwoahundatfünfadochzg")
+
+def test_numbers2words_minus1():
+    assert_raises(ValueError, lambda: numbers2words(-1))
